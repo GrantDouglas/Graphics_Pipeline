83e28e64341152015ed9ad43f55bc9ef45ff1139
Grant <grantdouglashand@outlook.com>
2018-01-19 15:35:16 -0500

    work on more

--- a1.py
+++ a1.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
-from PIL import Image, ImageDraw
-import math, time, sys
+import math, time, sys, array
+import numpy as np 
+from PIL import Image
 # import fbpy as fb
+
+
+class RGB:
+	def __init__(self, red, green, blue):
+		self.cRed = red
+		self.cBlue = blue
+		self.cGreen = green
+
+
 
 def xTransform(points, angle):
 	for i in points:
@@ -26,12 +36,32 @@ def yTransform(points, angle):
 		i[0] = newx
 		i[2] = newz
 
-def rasterize(matrix):
-	j = 0
-	for k in matrix:
-		j = 1
+def rasterize(point1, point2, image):
+	dx = abs(point2[0] - point1[0])
+	dy = abs(point2[1] - point1[1])
+
+	if dx >= dy:
+		length = dx
+	else:
+		length = dy
+
+	dx = (point2[0] - point1[0])/float(length)
+	dy = (point2[1] - point1[1])/float(length)
+
+	x = point1[0]
+	y = point1[1]
+	image[math.ceil(x)][math.ceil(y)] = 255
 
 
+	for i in range(length-1):
+		x += dx
+		y += dy
+		image[math.ceil(x)][math.ceil(y)] = 255
+
+def oneDimension(x, y):
+	return y * 800 + x
+
+np.set_printoptions(threshold=np.nan)
 
 angle = float(sys.argv[1])
 
@@ -48,25 +78,36 @@ points = [
 		[-128,-128,128]
 		]
 
-# angle = 3.1415/4
+angle = 3.1415/4
 
 # print(points)
 
 yTransform(points, angle)
 xTransform(points, angle)
 
-# print(points)
+# print(points) y * 800 + x
 
 matrix = []
 count = 0
 
+# add 300 to each point so its actually on the screen
 for k in points:
 	k[0] += 300
 	k[1] += 300
 	matrix.append(k[:2])
 
+image = [[0 for x in range(800)] for y in range(800)]
+
+
+
+# place a value of 255 on the image where the cube will be
+for i in matrix:
+	image[i[0]][i[1]] = 255
+	
+
 finalMatrix = [tuple(l) for l in matrix]
 
+# make a hashmap relating each point to its respective connections
 connections = {
 				finalMatrix[0]: [finalMatrix[1], finalMatrix[2], finalMatrix[4]],
 				finalMatrix[1]: [finalMatrix[3], finalMatrix[5], finalMatrix[0]],
@@ -79,26 +120,21 @@ connections = {
 }
 
 for key, val in connections.items():
+	for point in val:
+		rasterize(key, point, image)
 	print(val)
 
+print(image)
 
-print(finalMatrix)
+string = ''.join(str(e) for e in image)
 
-# print(matrix)
-# print("hello")
 
-img = Image.new('RGB', (800,800), (0, 0, 0))
-draw = ImageDraw.Draw(img)
+print(connections)
 
-for key, val in connections.items():
-	for x in val:
-		draw.line([key,x])
+arr = np.array(image)
 
-# draw.line(finalMatrix)
 
-# draw.polygon(finalMatrix)
-img.save("image.png", "PNG")
+print(arr)
 
-img.show()
-# im = Image.open("test.jpg")
-# im.show()
+
+
