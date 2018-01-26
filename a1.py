b9bd333ea285383978efbd5e8c0a01d51ef3b10f
Grant <grantdouglashand@outlook.com>
2018-01-25 19:52:01 -0500

    done for now

--- a1.py
+++ a1.py
@@ -3,10 +3,11 @@
 import math, time, sys, array, itertools
 import numpy as np
 from PIL import Image
-from random import shuffle
-# import fbpy as fb
 
 
+#
+#   This is a class used to store the RGB values of the image matrix
+#
 class RGB:
     def __init__(self, red, green, blue):
         self.cRed = red
@@ -18,8 +19,11 @@ class RGB:
         print(self.cBlue)
         print(self.cGreen)
 
-
+#
+#  This is a class that stores all of the coordinates for the points.
+#
 class coords:
+
     def __init__(self, x, y, z):
         self.xCoord = x
         self.yCoord = y
@@ -46,7 +50,11 @@ class coords:
     def get_z(self):
         return self.zCoord
 
-
+#
+#   This function is responsible for transforming the given points through the x axis.
+#   In: points  A list of points to be transformed
+#       angle   An angle in radians to rotate the x axis by
+#
 def xTransform(points, angle):
     for i in points:
         y = i.get_y()
@@ -60,6 +68,15 @@ def xTransform(points, angle):
 
 
 def yTransform(points, angle):
+    """Transform points in the y axis
+
+    This function will transform a given list of points on the y axis by a given angle
+
+    Arguments:
+        points {List} -- A list of points to bhe rotated
+        angle {Float} -- An angle in radians to rotate the axis by
+    """
+
     for i in points:
         x = i.get_x()
         z = i.get_z()
@@ -71,19 +88,16 @@ def yTransform(points, angle):
         i.set_z(newz)
 
 
-def zTransform(points, angle):
-    for i in points:
-        x = i[0]
-        y = i[1]
-
-        newx = int(x * math.cos(angle) + y * math.sin(angle))
-        newy = int(y * math.cos(angle) - x * math.sin(angle))
-
-        i[0] = newx
-        i[1] = newy
-
-
 def rasterize(point1, point2, image):
+    """Rasterize a line between two points
+
+    This function will rasterize a line between two points using the DDA
+
+    Arguments:
+        point1 {coord} -- A coord object which specifies the first point to rasterize
+        point2 {coord} -- A coord object which specifies the second point to rasterize
+        image {List of RGB} -- The image matrix which stores each points RGB value
+    """
     dx = abs(point2.get_x() - point1.get_x())
     dy = abs(point2.get_y() - point1.get_y())
 
@@ -111,7 +125,19 @@ def rasterize(point1, point2, image):
         image[math.ceil(x)][math.ceil(y)].cBlue = 255
 
 
-def connect(matrix, resolution, key):
+def connect(matrix, key):
+    """creates a connection between a point and all neighbouring points
+
+    This function will create a connection between a point and multiple others
+    by checking their adjacency and their plane.
+
+    Arguments:
+        matrix {list of coords} -- A matrix of objects which are in the vector space
+        key {coord} -- A coord object which contains the coordinate information of the key
+
+    Returns:
+        List of coords -- A list of coord objects which are mapped to the key
+    """
     connectionPoints = []
 
     for x in matrix:
@@ -129,17 +155,32 @@ def connect(matrix, resolution, key):
 
     return connectionPoints
 
+
 def circleCoords(startX, startY, radius):
+    """Creates a circle
+
+    This function will create a circle using the Midpoint Circle Algorithm
+
+    Arguments:
+        startX {int} -- The starting x position of the circle
+        startY {int} -- The starting y position of the circle
+        radius {int} -- The radius of the circle
+
+    Returns:
+        List -- A list of coordinates which create a circle
+    """
     x = radius - 1
     y = 0
 
-    dx = 1
-    dy = 1
+    delta_x = 1
+    delta_y = 1
 
-    error = dx - (radius << 1)
+    error = delta_x - (radius * 2)
 
     points = []
+    points.append([128, 0, -128])
 
+    # plot all points until it hits the y axis
     while x >= y:
         points.append([startX + x, startY + y, -128])
         points.append([startX + y, startY + x, -128])
@@ -150,21 +191,64 @@ def circleCoords(startX, startY, radius):
         points.append([startX + y, startY - x, -128])
         points.append([startX + x, startY - y, -128])
 
+        # move the y coordinate up if it did not go far enough. otherwise move back an x coordinate
         if error <= 0:
             y += 1
-            error += dy
-            dy += 2
+            error += delta_y
+            delta_y += 2
         else:
             x -= 1
-            dx += 2
-            error += dx - (radius << 1)
+            delta_x += 2
+            error += delta_x - (radius * 2)
 
     return points
 
 
+def connectCircle(res, key):
+    """Divides a circle into even portions
+
+    This function will divide a circle into portions based on the resolution value
+
+    Arguments:
+        resolution {int} -- The triangle resolution specified by the user
+        key {coord} -- The centre point of the circle
+
+    Returns:
+        list of coords -- A list of coord objects which make up the circle division
+    """
+    r = 128
+    angle = 0
+    connectList = []
+
+    for i in range(0, res):
+
+        # create an angle between 0 and 2pi that is split evenly by the resolution number
+        angle = i * (6.28 / res)
+
+        # calculate new x and y coordinates that meet the angle specified.
+        x = int(key.get_x() + r * math.cos(angle)) + 300
+        y = int(key.get_y() + r * math.sin(angle)) + 400
+        connectList.append(coords(x, y, -128))
+
+    return connectList
+
+
 def sphere(resolution, mesh):
+    """Creates a sphere
+    This function will create a sphere
+
+    Arguments:
+        resolution {int} -- The number of triangles to create
+        mesh {string} -- The type of mesh to create
+
+    Returns:
+        List -- A dictionary where each key is a coord object, and it maps to a list of coord objects
+        List -- A matrix which contains coord objects for each point of the sphere
+    """
     points = circleCoords(128, 0, 128)
     finalPoints = []
+    matrix = []
+    connections = {}
     for x in points:
         if x not in finalPoints:
             finalPoints.append(x)
@@ -172,37 +256,40 @@ def sphere(resolution, mesh):
     coordList = [coords(x[0], x[1], x[2]) for x in finalPoints]
 
     for k in coordList:
+        k.set_x(k.xCoord)
+        k.set_y(k.yCoord)
+        k.set_z(k.zCoord)
+        matrix.append(k)
+
+    coordSet = [x for x in matrix]
+
+    print(coordSet[0].send_vals())
+
+    connections[coordSet[0]] = connectCircle(resolution, coordSet[0])
+
+    print(connections)
+
+    for k in matrix:
         k.set_x(k.xCoord+300)
-        k.set_y(k.yCoord+300)
-        k.set_z(k.zCoord+300)
+        k.set_y(k.yCoord+400)
+        k.set_z(k.zCoord+400)
 
-    print(points)
-
-    image = [[RGB(0, 0, 0) for x in range(800)] for y in range(800)]
-
-    # place a value of 255 on the image where the cube will be
-    for i in coordList:
-        image[i.get_x()][i.get_y()].cRed = 255
-        image[i.get_x()][i.get_y()].cGreen = 255
-        image[i.get_x()][i.get_y()].cBlue = 255
-
-    # rasterize a line between the key and each of its respective points
-
-    newImage = image
-
-    for m, i in enumerate(image):
-        for n, j in enumerate(i):
-            newImage[m][n] = (j.cRed, j.cGreen, j.cBlue)
-
-    arr = np.array(image, dtype=np.uint8)
-
-    # print(arr)
-
-    img = Image.fromarray(arr, 'RGB')
-    img.save('testing.png')
+    return connections, matrix
 
 
 def cube(res, mesh):
+    """Create a cube
+
+    This function will create the points which make up a cube
+
+    Arguments:
+        res {int} -- The resolution of the faces on the cube
+        mesh {string} -- the mesh type of the cube
+
+   Returns:
+        List -- A dictionary where each key is a coord object, and it maps to a list of coord objects
+        List -- A matrix which contains coord objects for each point of the sphere
+    """
     points = [
         [128, 128, -128],
         [-128, 128, -128],
@@ -214,8 +301,8 @@ def cube(res, mesh):
         [-128, -128, 128]
         ]
 
-    if mesh == "triag":
-        triangGrid = triangleMesh(res, points)
+    if mesh == "tri":
+        triangGrid = grid(res, points)
         newPoints = triangGrid + points
     elif mesh == "poly":
         newPoints = points
@@ -227,44 +314,56 @@ def cube(res, mesh):
     matrix = []
     connections = {}
 
+    # create a list of unique points
     for x in newPoints:
         if x not in finalPoints:
             finalPoints.append(x)
 
     coordList = [coords(x[0], x[1], x[2]) for x in finalPoints]
 
-    connect(coordList, resolution, coordList[0])
-
+    # store the points in a matrix
     for k in coordList:
         k.set_x(k.xCoord)
         k.set_y(k.yCoord)
         k.set_z(k.zCoord)
         matrix.append(k)
 
-    for x in matrix:
-        print(x.send_vals())
-
+    # create a set out of these coordinates to use as keys for the connection dictionary
     coordSet = [x for x in matrix]
 
+    # create a dictionary which maps each point to a list of connected points
     for key in coordSet:
-        connections[key] = connect(coordList, resolution, key)
+        connections[key] = connect(coordList, key)
 
     # transform the x and y coordinates to make 3d shape more clear
     yTransform(matrix, angle)
     xTransform(matrix, angle)
 
+    # move the square to the middle of the image for a cleaner look
     for k in coordList:
         k.set_x(k.xCoord+300)
-        k.set_y(k.yCoord+300)
-        k.set_z(k.zCoord+300)
+        k.set_y(k.yCoord+400)
+        k.set_z(k.zCoord+400)
 
     return connections, matrix
 
 
-def triangleMesh(resolution, points):
+def grid(resolution, points):
+    """Create a grid on a square surface
+
+    This function will create a grid of a specified resolution onto a surface identified by coordinates
+
+    Arguments:
+        resolution {int} -- The resolution of the grid
+        points {list} -- The list of points that make up the cube
+
+    Returns:
+        list -- A list of points which
+    """
     new = list(itertools.combinations(points, 2))
     final = []
 
+    # find all unique pair combinations within the point list
     for x in new:
         if (x[1][0] - x[0][0] == 0 and x[1][2] == x[0][2]) or (x[1][1] - x[0][1] == 0 and x[1][2] == x[0][2]) or (x[1][2] - x[0][2] != 0 and x[1][1] == x[0][1] and x[1][0] == x[0][0]):
             final.append(x)
@@ -272,6 +371,8 @@ def triangleMesh(resolution, points):
     newFinal = []
 
     for x in final:
+
+        # place grid points on z axis lines if the z axis are equal
         if x[1][2] - x[0][2] != 0:
             gap = int((abs(x[0][2]) + abs(x[1][2])) / resolution)
             start = min(x[0][2], x[1][2])
@@ -280,6 +381,7 @@ def triangleMesh(resolution, points):
             for k in range(start, end, gap):
                 newFinal.append([x[0][0], x[0][1], k])
 
+        # place grid points on y axis lines if the x axis are equal
         elif x[1][0] - x[0][0] == 0:
             gap = int((abs(x[0][1]) + abs(x[1][1])) / resolution)
             start = min(x[0][1], x[1][1])
@@ -288,6 +390,7 @@ def triangleMesh(resolution, points):
             for k in range(start, end, gap):
                 newFinal.append([x[0][0], k, x[0][2]])
 
+        # place grid points on x axis lines if the y axis are equal
         elif x[1][1] - x[0][1] == 0:
             gap = int((abs(x[0][0]) + abs(x[1][0])) / resolution)
             start = min(x[0][0], x[1][0])
@@ -310,6 +413,7 @@ if __name__ == "__main__":
     mesh = sys.argv[3]
     resolution = int(sys.argv[4])
 
+    # redirect to appropriate algorithm for the shape
     if shape == "cube":
         connections, matrix = cube(resolution, mesh)
     elif shape == "sphere":
@@ -334,11 +438,12 @@ if __name__ == "__main__":
 
     newImage = image
 
+    # create a new image matrix with just the colour values of the cells
     for m, i in enumerate(image):
         for n, j in enumerate(i):
             newImage[m][n] = (j.cRed, j.cGreen, j.cBlue)
 
-    arr = np.array(image, dtype=np.uint8)
+    arr = np.array(newImage, dtype=np.uint8)
 
     # print(arr)
 
